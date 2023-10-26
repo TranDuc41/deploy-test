9b2ec07900678e3b932ea4efbc1acdc9076d1a72
TranDuc41 <td07197@gmail.com>
2023-10-26 09:56:58 +0700

    fix bug header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,9 +1,9 @@
-'use client'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import Link from 'next/link';
+import { Button } from 'react-bootstrap';
 
 const links = [
   { href: '/', text: 'Trang chủ' },
@@ -16,23 +16,28 @@ const links = [
 ];
 
 function AppHeader() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavItemClick = () => {
+    // Khi bạn nhấp vào một liên kết, hãy đặt expanded về giá trị false để đóng collapse.
+    setExpanded(false);
+  };
+
   return (
-    <Navbar fixed="top" collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar fixed="top" collapseOnSelect expand="lg" className="bg-body-tertiary" expanded={expanded}>
       <Container>
-        <Nav className="me-auto">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        </Nav>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end flex-wrap'>
         <Nav>
-        <span>Địa chỉ: 123 Đường ABC, Thành phố XYZ</span>
-        <span>Email: example@example.com</span>
-        <span>Điện thoại: 0123 456 789</span>
-      </Nav>
+            <span>Địa chỉ: 123 Đường ABC, Thành phố XYZ</span>
+            <span>Email: example@example.com</span>
+            <span>Điện thoại: 0123 456 789</span>
+          </Nav>
           <Nav>
             {links.map((link, index) => (
-              <Link key={index} href={link.href} className='nav-link me-3'>
-                {link.text}
+              <Link key={index} href={link.href}className='nav-link me-3' onClick={handleNavItemClick}>
+                  {link.text}
               </Link>
             ))}
             <Button>Đặt Ngay</Button>
