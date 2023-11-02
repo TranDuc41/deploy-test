c3d6340daca6d58100d69c90768e6ee4512fc7d6
TranDuc41 <td07197@gmail.com>
2023-11-02 08:06:30 +0700

    create banner booking and filter

--- frontend/src/components/bannerBooking.js
+++ frontend/src/components/bannerBooking.js
@@ -1,33 +1,35 @@
 'use client'
 import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import Form from 'react-bootstrap/Form';
 
-const titles = [
-    { text: "Nhận Phòng" },
-    { text: "Trả Phòng" },
-]
-
-export default function MyDatePicker() {
+function BannerBooking() {
     const [startDate, setStartDate] = useState(new Date());
 
     return (
         <div className='categories-wrap d-flex justify-content-evenly'>
             <div className='row align-items-center justify-content-between w-100'>
-                {titles.map((title, index) => (
-                    <div key={index} className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
-                        <div className='cate-items'>
-                            <h5>{title.text}</h5>
-                            <DatePicker
-                                selected={startDate}
-                                onChange={(date) => setStartDate(date)}
-                                dateFormat="d/M/y"
-                            />
-                        </div>
+                <div className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
+                    <div className='cate-items'>
+                        <h5>Ngày Nhận</h5>
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            dateFormat="M/d/y"
+                        />
                     </div>
-                ))}
+                </div>
+                <div className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
+                    <div className='cate-items'>
+                        <h5>Ngày Trả</h5>
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            dateFormat="M/d/y"
+                        />
+                    </div>
+                </div>
                 <div className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
                     <div className='cate-items'>
                         <h5>Người Lớn</h5>
@@ -48,20 +50,9 @@ export default function MyDatePicker() {
                         </Form.Select>
                     </div>
                 </div>
-                <div className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
-                    <div className='cate-items'>
-                        <h5>Phòng</h5>
-                        <Form.Select aria-label="Default select example">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                        </Form.Select>
-                    </div>
-                </div>
-                <div className='col-xxl-2 col-xl-4 col-lg-4 col-md-4 col-sm-4'>
-                    <Button className='px-4 btn-check-now w-100'>Đặt Ngay</Button>
-                </div>
             </div>
         </div>
     );
 }
+
+export default BannerBooking;
