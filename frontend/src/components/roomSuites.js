8fbe57221605de0fea1ce9fcd0918dc279489cf7
Trần Đức <80045331+TranDuc41@users.noreply.github.com>
2024-03-05 08:21:54 -0800

    Update roomSuites.js

--- frontend/src/components/roomSuites.js
+++ frontend/src/components/roomSuites.js
@@ -58,7 +58,7 @@ const rooms = [
     }
 
 ]
-export default function roomsuites() {
+export default function RoomSuites () {
     const [slidesPerView, setSlidesPerView] = useState(5); // Giá trị mặc định cho desktop
 
     useEffect(() => {
